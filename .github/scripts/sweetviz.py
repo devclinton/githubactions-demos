c82cf7d7fa37b01e38449a83e918f87ff52f3961
Clinton Collins <devclinton@users.noreply.github.com>
2023-07-17 09:59:34 -0700

    Fix data examples

--- .github/scripts/sweetviz.py
+++ .github/scripts/sweetviz.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import argparse
-import sweetviz as sv
+
+
 
 
 if __name__ == "__main__":
@@ -11,6 +12,8 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     df = pd.read_csv(args.file, header=args.header, index_col=[0])
+
+    import sweetviz as sv
     report = sv.analyze(df, "Label")
     # save file in html format
     report.show_html(f"{args.file}.sweetviz.html")
